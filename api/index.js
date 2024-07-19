ddb55945369f9c49ac3c891911fd5a1f46fe5105
Nikhil <nngg525252@gmail.com>
2024-07-19 21:33:39 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,16 +2,22 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.routes.js'
+import authRouter from './routes/auth.routes.js'
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log("Connected to MONGODB");
-}).catch((err)=>{
-    console.log(err);
+mongoose.connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.error("Database connection error:", err);
 });
 const app=express();
 
+app.use(express.json());
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
 app.listen(3000,()=>{
 console.log('Server running on port 3000');
 });
