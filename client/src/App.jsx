51a6c034a94715ef6998f631288241ebd9fd4db8
Nikhil <nngg525252@gmail.com>
2024-07-22 22:16:53 +0530

    Create createlisting ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Profile from './pages/Profile';
 import SignUp from './pages/Signup';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+import Createlisting from './pages/CreateListing';
 
 export default function App() {
   return (
@@ -20,6 +21,7 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
+          <Route path='/create-listing' element={<Createlisting />} />
         </Route>
 
       </Routes>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -17,7 +17,7 @@ import {
   signOutUserStart,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
-
+import {Link} from "react-router-dom";
 export default function Profile() {
   const { currentUser ,loading, error } = useSelector((state) => state.user);
   const fileRef=useRef(null);
@@ -180,7 +180,12 @@ export default function Profile() {
         >
           {loading ? 'Loading...' : 'Update'}
         </button>
-
+        <Link
+          className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+          to={'/create-listing'}
+        >
+          Create Listing
+        </Link>
 
 
   </form>
